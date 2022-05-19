f998eec8152ce23d9541fc63a12def7e3974193d
André Luiz H. Kanashiro <alhkanashiro@gmail.com>
2022-05-19 10:25:05 -0300

    Add JS aula

---
+++ 4-  Java Script/scripts.js
@@ -0,0 +1,20 @@
+
+function teste() {
+    // var idade = 19;
+    // var nome = "Leonardo";
+    // var sobrenome;
+    // sobrenome = "Hitner"
+
+    // document.getElementById("info").innerHTML = "Meu nome é " + nome + " " + sobrenome + " e tenho " + idade + " anos.";
+
+    // var nomes= ["João", "Carlos" , "Ana"]
+
+    // document.getElementById("info").innerHTML = nomes[1];
+
+    var automovel = {
+    marca: "Ford", 
+    ano: 2021, 
+    cor: "Preto"}
+
+    document.getElementById("info").innerHTML = automovel.ano;
+} 
